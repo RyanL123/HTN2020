33f8448707a6d32063ff91e18d9466798c04bd3f
Ryan <ryan.lichangrui@gmail.com>
2021-01-16 15:33:03 -0500

    fix import error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-import * as firebase from "firebase";
-
-const config = {
-    apiKey: "AIzaSyCUMKP7f1rcG94J_Bb2TpFXA4lyL-aAMk4",
-    authDomain: "htn2020-c3c0b.firebaseapp.com",
-    projectId: "htn2020-c3c0b",
-    storageBucket: "htn2020-c3c0b.appspot.com",
-    messagingSenderId: "352855450184",
-    appId: "1:352855450184:web:bfe901aebf130800de2e7d",
-};
-
-firebase.initializeApp(config);
 
 function App() {
     return (
