2fcc85c43c2a65327cb601466811a100876202df
Ryan <ryan.lichangrui@gmail.com>
2021-01-16 15:30:43 -0500

    intergrate firestore

--- functions/voiceflow.js
+++ functions/voiceflow.js
@@ -1,8 +1,28 @@
+const firebase = require("firebase");
+
+const config = {
+    apiKey: "AIzaSyCUMKP7f1rcG94J_Bb2TpFXA4lyL-aAMk4",
+    authDomain: "htn2020-c3c0b.firebaseapp.com",
+    projectId: "htn2020-c3c0b",
+    storageBucket: "htn2020-c3c0b.appspot.com",
+    messagingSenderId: "352855450184",
+    appId: "1:352855450184:web:bfe901aebf130800de2e7d",
+};
+
+firebase.initializeApp(config);
+
+const ref = firebase.firestore().collection("data");
+
 exports.handler = async function (event, context, callback) {
-    const { number } = JSON.parse(event.body);
-    console.log(number);
-    return {
-        statusCode: 200,
-        body: "Number:" + number.toString(),
-    };
+    console.log(ref);
+    if (event.httpMethod === "POST") {
+        const { number } = JSON.parse(event.body);
+        console.log(number);
+        console.log(event);
+        const res = await ref.doc("test1").set({ number: number });
+        return {
+            statusCode: 200,
+            body: "Number: " + number.toString(),
+        };
+    }
 };
--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as firebase from "firebase";
+
+const config = {
+    apiKey: "AIzaSyCUMKP7f1rcG94J_Bb2TpFXA4lyL-aAMk4",
+    authDomain: "htn2020-c3c0b.firebaseapp.com",
+    projectId: "htn2020-c3c0b",
+    storageBucket: "htn2020-c3c0b.appspot.com",
+    messagingSenderId: "352855450184",
+    appId: "1:352855450184:web:bfe901aebf130800de2e7d",
+};
+
+firebase.initializeApp(config);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
