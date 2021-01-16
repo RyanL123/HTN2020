26c48657b687988cedea4bbeb819d4b90f4bc485
Ryan <ryan.lichangrui@gmail.com>
2021-01-16 14:29:13 -0500

    testing netlify functions body

--- functions/voiceflow.js
+++ functions/voiceflow.js
@@ -1,6 +1,3 @@
 exports.handler = async function(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({event: event})
-    };
+    console.log(JSON.parse(event.body))
 }
