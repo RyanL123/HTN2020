c048c12d40010b83c6be1f091b9ed9b38dc025e2
Ryan <ryan.lichangrui@gmail.com>
2021-01-17 10:20:53 -0500

    add event endpoint

--- functions/voiceflow.js
+++ functions/voiceflow.js
@@ -1,6 +1,4 @@
 const firebase = require("firebase")
-const shortid = require("shortid")
-
 const config = {
     apiKey: "AIzaSyCUMKP7f1rcG94J_Bb2TpFXA4lyL-aAMk4",
     authDomain: "htn2020-c3c0b.firebaseapp.com",
@@ -18,12 +16,30 @@ const ref = firebase.firestore().collection("data")
 
 exports.handler = async function (event, context, callback) {
     if (event.httpMethod === "POST") {
-        const { number } = JSON.parse(event.body)
-        const id = shortid.generate()
-        const res = await ref.doc(id).set({ number: number })
+        const {
+            name,
+            start_time,
+            end_time,
+            colorScheme,
+            session_id,
+        } = JSON.parse(event.body)
+        const res = await ref
+            .doc(session_id)
+            .set({ colorScheme: colorScheme, events: [] })
+        const res2 = await ref.doc(session_id).events.update({
+            start_time: start_time,
+            end_time: end_time,
+            name: name,
+        })
         return {
             statusCode: 200,
-            body: JSON.stringify({ id: id }),
+            body: JSON.stringify({
+                name: name,
+                start_time: start_time,
+                end_time: end_time,
+                colorScheme: colorScheme,
+                session_id: session_id,
+            }),
         }
     }
 }
