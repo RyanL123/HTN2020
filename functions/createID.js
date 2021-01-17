0508ec3aa272c6a60aa8cc504e2b48f4432f9606
Ryan <ryan.lichangrui@gmail.com>
2021-01-16 21:23:16 -0500

    add createID function

---
+++ functions/createID.js
@@ -0,0 +1,27 @@
+const firebase = require("firebase")
+
+const config = {
+    apiKey: "AIzaSyCUMKP7f1rcG94J_Bb2TpFXA4lyL-aAMk4",
+    authDomain: "htn2020-c3c0b.firebaseapp.com",
+    projectId: "htn2020-c3c0b",
+    storageBucket: "htn2020-c3c0b.appspot.com",
+    messagingSenderId: "352855450184",
+    appId: "1:352855450184:web:bfe901aebf130800de2e7d",
+}
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config)
+}
+
+const ref = firebase.firestore().collection("data")
+
+exports.handler = async function (event, context, callback) {
+    if (event.httpMethod === "POST") {
+        const { id } = JSON.parse(event.body)
+        const res = await ref.doc(id).set({ dateCreated: Date.now() })
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ id: id }),
+        }
+    }
+}
